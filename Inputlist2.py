261701ac5624b6d1e1ad76f17ca953d800ca057e
Nandkishor Dilip Wankhede <71113456+Nandy312@users.noreply.github.com>
2021-10-12 09:43:57 +0530

    Add files via upload

---
+++ Inputlist2.py
@@ -0,0 +1,13 @@
+# Python program to print positive Numbers in a List
+  
+# list of numbers
+list1 = [12, 14, -95, 3]
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+   
+
